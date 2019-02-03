0454cce6de860ad3e72eda1280532bdfada8a8f2
Monty <tuckerjsarah@gmail.com>
2019-02-03 12:32:44 -0500

    change up

--- assets/javascript.js
+++ assets/javascript.js
@@ -13,49 +13,79 @@ var database= firebase.database();
 var convo = "";
 var user1Picked = "";
 var user2Picked= "";
+var p1rot;
+var p2rot;
+// $("#signUp").click(function(e){
+//     e.preventDefault();
+//     var userName= $("#userName").val();
+//     var email = $("#emailInput").val();
+//     var password = $("#passwordInput").val();
+//     firebase.auth().createUserWithEmailAndPassword(email, password)
+//     .then(function(user){
+//         user.updateProfile({userName:userName});
+//     });
+//     $("#login").atrr("display", "none");
+// });
 
-$("#signUp").click(function(e){
-    e.preventDefault();
-    var userName= $("#userName").val();
-    var email = $("#emailInput").val();
-    var password = $("#passwordInput").val();
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-    .then(function(user){
-        user.updateProfile({userName:userName});
-    });
-    $("#userName").empty();
-    $("#emailInput").empty();
-    $("#passwordInput").empty();
-    console.log("click");
-});
+// $('#signIn').click(function(e){
+//     e.preventDefault();
+//     var email = $("#emailInput").val();
+//     var password = $("#passwordInput").val();
+//     firebase.auth().signInWithEmailAndPassword(email, password);
+//     $("#userName").empty();
+//     $("#emailInput").empty();
+//     $("#passwordInput").empty(); 
+//     $("#login").attr("display", "none");
+// });
+var connectionsRef = database.ref("/connections");
+var connectedRef = database.ref(".info/connected");
+connectedRef.on("value", function(snap) {
 
-$('#signIn').click(function(e){
-    e.preventDefault();
-    var email = $("#emailInput").val();
-    var password = $("#passwordInput").val();
-    firebase.auth().signInWithEmailAndPassword(email, password);
-    $("#userName").empty();
-    $("#emailInput").empty();
-    $("#passwordInput").empty(); 
-    console.log("click");
+    // If they are connected..
+    if (snap.val()) {
+  
+      // Add user to the connections list.
+      var con = connectionsRef.push(true);
+  
+      // Remove user from the connection list when they disconnect.
+      con.onDisconnect().remove();
+    }
+  });
+  // When first loaded or when the connections list changes...
+connectionsRef.on("value", function(snapshot) {
+    if(snapshot.numChildren()==1){
+        $("#status").text("wait for opponent");
+    }else if (snapshot.numChildren()==2){
+        $("#status").text("Play!");
+    }else if (snapshot.numChildren()>2){
+        $("#status").text("Wait your turn...");
+    }
+  });
 
-});
+// firebase.auth().onAuthStateChanged(function(user) {
+//     if (user) {
+     
+//         databas.ref("/player").push({
+//             player: firebase.auth().currentUser
+//         }) 
+//     } else {
+      
+//     }
+//   });
+
 
 
 $("#addConvo").click(function(error){
     error.preventDefault();
     convo = $("#chat-input").val();
-    database.ref("/chat").push().set({
-        name:firebase.auth().currentUser.userName,
+    database.ref("/chat").push({
         convo:convo
     });
     $("#chat-input").html(" ");
 });
 database.ref("/chat").on("child_added", function(snapshot){
     var snap= snapshot.val();
-    console.log(snap.name);
     var div = $("<div>")
-    div.text(snap.name + ": ");
     div.append(snap.convo);
     $("#convo").append(div)
 },
@@ -65,30 +95,38 @@ function(errObjects){
 )
 
 
-$("#playBtn1").click(function(){
-    $("#playBtn1").css("display", "none");
-    setInterval(rotate, 2500);
+$("#playBtn").click(function(){
+    $("#playBtn").css("display", "none");
+    p1rot= setInterval(rotate, 2500);
     function rotate(){
         $("#rps1 div").last().fadeOut(1000, function(){
             $(this).insertBefore($("#rps1 div").first()).show();
         });
-    }
-});   
-$("#rpsPic").click(function(){
-    user1Picked=$(this).attr("data-name").val()
-})
 
-$("#playBtn2").click(function(){
-    $("#playBtn2").css("display", "none");
-    setInterval(rotate, 2500);
+    }
+});  
+
+$("#play2Btn").click(function(){
+    $("#play2Btn").css("display", "none");
+    p2rot= setInterval(rotate, 2500);
     function rotate(){
         $("#rps2 div").last().fadeOut(1000, function(){
             $(this).insertBefore($("#rps2 div").first()).show();
         });
     }
-});   
-$("#rpsPic").click(function(){
+})
+
+
+$("#rps1Pic").click(function(){
+    user1Picked=$(this).attr("data-name").val()
+    clearInterval(p1rot);
+    $("#rps1").css("display", "none");
+})
+
+$("#rps2Pic").click(function(){
     user2Picked=$(this).attr("data-name").val()
+    clearInterval(p2rot);
+    $("#rps2").css("display", "none");
 })
 
 // firebase.auth().onAuthStateChanged(authStateChangedListener);
